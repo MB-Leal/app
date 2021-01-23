764b1e6f952e2487da80e6ae4021bb5bcbb4cadd
Glitch (hello-express) <none>
2021-01-23 15:37:37 +0000

    🐦🚀 Checkpoint ./server.js:13141528/1574

--- server.js
+++ server.js
@@ -234,24 +234,19 @@ if(intentName == 'Consulta_Planilha'){
  });
  });*/
     
-    <script src="//cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.js"></script>
-    // Get all data
-    axios.get('https://sheetdb.io/api/v1/58f61be4dda40')
-    .then( response => {
-        console.log(response.data);
+    function getSpreadsheetData(){
+  	return axios.get('https://sheetdb.io/api/v1/7nvryedhnduyg');
+  }
+  
+  function welcome(agent) {
+    const nome = agent.parameters.['nome'];
+    return getSpreadsheetData().then(res => {
+    	res.data.map(person => {
+            if(person.Nome === nome)
+        	agent.add('Seu nome ${nome}. Telefone: ${person.Telefone}, Data: ${person.Data}');
+        });
     });
-
-    // Get 10 results starting from 20
-    axios.get('https://sheetdb.io/api/v1/58f61be4dda40?limit=10&offset=20')
-    .then( response => {
-        console.log(response.data);
-    });
-
-    // Get all data sorted by name in ascending order
-    axios.get('https://sheetdb.io/api/v1/58f61be4dda40?sort_by=name&sort_order=asc')
-    .then( response => {
-        console.log(response.data);
-    });
+  }
  }
   ///==============================================================================
   
