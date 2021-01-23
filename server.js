d15889c00f05654672d3de202277590f2efd292e
Glitch (hello-express) <none>
2021-01-23 15:25:39 +0000

    🐂🚎 Checkpoint ./server.js:13141528/1065

--- server.js
+++ server.js
@@ -212,20 +212,46 @@ if(intentName == 'Consulta_Planilha'){
  } 
   
   if(intentName == 'ListarPlanilha'){
+    /*axios.get('https://sheetdb.io/api/v1/7nvryedhnduyg').then( res => {
+        console.log(response.data);
+      response.json({"fulfillmentText" : "Resposta "+res.data});
+                    });
+    }
+    
+  
   var Nome = request.body.queryResult.parameters['nome'];
   var resNome = Nome.toUpperCase();
   return axios.get("https://sheetdb.io/api/v1/7nvryedhnduyg").then(res => {
  res.data.map(person => {
    
- /*if (person.Nome === resNome){
+ if (person.Nome === resNome){
  response.json({"fulfillmentText" :"Detalhes do cadastro: "+
  "Nome: "+person.Nome+"\n"+
  "Telefone: "+person.Telefone+"\n"+
  "Data Cadastro: "+person.Data });   
- }*/
+ }
    
  });
- });
+ });*/
+    
+    <script src="//cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.js"></script>
+    // Get all data
+    axios.get('https://sheetdb.io/api/v1/58f61be4dda40')
+    .then( response => {
+        console.log(response.data);
+    });
+
+    // Get 10 results starting from 20
+    axios.get('https://sheetdb.io/api/v1/58f61be4dda40?limit=10&offset=20')
+    .then( response => {
+        console.log(response.data);
+    });
+
+    // Get all data sorted by name in ascending order
+    axios.get('https://sheetdb.io/api/v1/58f61be4dda40?sort_by=name&sort_order=asc')
+    .then( response => {
+        console.log(response.data);
+    });
  }
   ///==============================================================================
   
